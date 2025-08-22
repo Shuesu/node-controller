9e9ca62c7b92d30dda259a07720fa8261bb075c6
Atabayev Ali <sptmbrw@gmail.com>
2025-08-22 19:17:39 +0500

    maybe nodemodule

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-const yargs = require('yargs');
-const pkg = require('./package.json');
-const {addNote, printNotes, removeNote} = require('./notes.controller');
+const http = require('http');
+const chalk = require('chalk');
+const fs = require('fs/promises');
+const path = require('path');
+const {addNote} = require('./notes.controller');
 
-yargs.version(pkg.version);
+const port = 3000;
 
-yargs.command({
-    command: 'add',
-    describe: 'Add new note to list',
-    builder:  {
-        title: {
-            type: 'string',
-            describe: 'Note title',
-            demandOption: true,
-        }
-    },
-    handler({title}) {
-        addNote(title);
+const basePath = path.join(__dirname, 'pages');
+
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET') {
+    const content = await fs.readFile(path.join(basePath, 'index.html'))
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+    } else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200,{'Content-Type': 'text/plain; charset=utf-8' });
+
+        req.on('data', data => {
+            body.push(Buffer.from(data));
+        })
+
+        req.on('end', () => {
+            const title = body.toString().split('=')[1].replaceAll('+', ' ')
+            addNote(title);
+            res.end(`Title = ${title}`);
+        })
+
+
     }
 })
 
-yargs.command({
-    command: 'list',
-    describe: 'Print all notes',
-    async handler() {
-        printNotes()
-    }
+server.listen(port, () => {
+    console.log(chalk.green(`Server is running on port: ${port}`));
 })
-
-yargs.command({
-    command: 'remove',
-    describe: 'Remove note by id',
-    builder:  {
-        id: {
-            type: 'string',
-            describe: 'Note uniq id',
-            demandOption: true,
-        }
-    },
-    handler({id}) {
-        removeNote(id);
-    }
-})
-
-
-yargs.parse()
